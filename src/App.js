a9118048adfc613b829c65a41ea1b41a626e9cdc
leegyotaek <leegyotaek>
2021-11-04 23:29:46 +0900

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [todos , setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out', 'Drink enough water']);
+  const [input, setInput] = useState('');
+
+  console.log('🔫)', input)
+
+  const addTodo = (event=> {
+
+    event.preventDefault();
+    console.log('👻', 'Im working');
+
+    setTodos([...todos, input])
+    setInput(''); // clear up the input after clicking add todo button
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World! 🚀🚀🚀</h1>
+
+      <form>
+
+      
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button type="submit" onClick={addTodo}>Add Todo</button>  
+      
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
 
 export default App;
+
